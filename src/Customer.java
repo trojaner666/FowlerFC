bc745e583c030adae2a5f7d3be87d3b65434c7e9
Patrick Fath <fath.patrick@yahoo.de>
2015-04-21 11:40:39 +0200

    Revert "moved amountFor(Rental aRental) from 'Customer.class' to getCharged() in"
    This reverts commit bafd9244d13737383ec6414748bf449507bfe41b.

--- src/Customer.java
+++ src/Customer.java
@@ -40,10 +40,26 @@ class Customer {
 				result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 				return result;
 	}
-	
-	//changed amountFor
+
+	// Rename "each" into "aRental" and "thisAmount" into "result"
 	private double amountFor(Rental aRental) {
-		return aRental.getCharge();
+		double result = 0;
+		switch (aRental.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (aRental.getDaysRented() > 2)
+				result += (aRental.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += aRental.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (aRental.getDaysRented() > 3)
+				result += (aRental.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return result;
 	}
-	
+
 }
