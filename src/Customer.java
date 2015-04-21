7398472f9c964d2b42a1cadd82861e669687b4a9
Patrick Fath <fath.patrick@yahoo.de>
2015-04-21 12:12:57 +0200

    changed in Costumer: this.amount to each.charge()

--- src/Customer.java
+++ src/Customer.java
@@ -17,29 +17,30 @@ class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Enumeration enum_rentals = rentals.elements();	    
-		String result = "Rental Record for " + this.getName() + "\n";
-		result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
-
+		Enumeration enum_rentals = rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
 		while (enum_rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = (Rental) enum_rentals.nextElement();
-			//determine amounts for each line
-			thisAmount = amountFor(each);
 			// add frequent renter points
 			frequentRenterPoints ++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-				frequentRenterPoints ++;
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&&
+					each.getDaysRented() > 1) frequentRenterPoints ++;
 			//show figures for this rental
-			result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle()+ "\t" +
+					String.valueOf
+					(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		//add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		result += "Amount owed is " + String.valueOf(totalAmount) +
+				"\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints)
+				+ " frequent renter points";
 		return result;
 	}
+
 	// Rename "each" into "aRental" and "thisAmount" into "result"
 	private double amountFor(Rental aRental) {
 		return aRental.getCharge();
--- src/Rental.java
+++ src/Rental.java
@@ -30,7 +30,4 @@ class Rental {
 		}
 		return result;
 	}
-
-
-
 }
